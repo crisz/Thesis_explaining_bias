a13c704a6cc13319cfdcb8530959c9b6c25725f2
crisz <crissstian96@gmail.com>
2021-11-01 18:09:52 +0100

    First implementation of train loop

--- src/0_setup_dataset_and_fine_tuning.py
+++ src/0_setup_dataset_and_fine_tuning.py
@@ -1,54 +1,105 @@
 import numpy as np
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler
+from tqdm import tqdm
 
 from dataset_utils.load_sst2 import load_train_dataset
-from transformers import BertTokenizer
+from transformers import BertTokenizer, BertForSequenceClassification, AdamW
 import torch
 
+from utils.cuda import get_device
 
-def main():
-    train_labels, train_sentences = load_train_dataset()
 
-    # Tokenizer
+def get_tokens_from_sentences(sentences):
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
 
-    # Tokenize all of the sentences and map the tokens to thier word IDs.
+    # Tokenize all of the sentences and map the tokens to their word IDs.
     input_ids = []
     attention_masks = []
 
-    for sentence in train_sentences:
-        # `encode_plus` will:
-        #   (1) Tokenize the sentence.
-        #   (2) Prepend the `[CLS]` token to the start.
-        #   (3) Append the `[SEP]` token to the end.
-        #   (4) Map tokens to their IDs.
-        #   (5) Pad or truncate the sentence to `max_length`
-        #   (6) Create attention masks for [PAD] tokens.
+    for sentence in sentences:
         encoded_dict = tokenizer.encode_plus(
             sentence,  # Sentence to encode.
             add_special_tokens=True,  # Add '[CLS]' and '[SEP]'
+            truncation=True,
             max_length=64,  # Pad & truncate all sentences.
-            pad_to_max_length=True,
+            padding='max_length',
             return_attention_mask=True,  # Construct attn. masks.
             return_tensors='pt',  # Return pytorch tensors.
         )
 
-        # Add the encoded sentence to the list.
         input_ids.append(encoded_dict['input_ids'])
-
-        # And its attention mask (simply differentiates padding from non-padding).
         attention_masks.append(encoded_dict['attention_mask'])
-        # Convert the lists into tensors.
-        input_ids = torch.cat(input_ids, dim=0)
-        attention_masks = torch.cat(attention_masks, dim=0)
-        print(train_labels.shape)
 
-    # labels = torch.tensor(np.array(train_labels, dtype=torch.float16))
+    # Convert the lists into tensors.
+    input_ids = torch.cat(input_ids, dim=0)
+    attention_masks = torch.cat(attention_masks, dim=0)
+    return input_ids, attention_masks
 
-    # Print sentence 0, now as a list of IDs.
-    print('Original: ', train_sentences[1])
-    print('Token IDs:', input_ids[1])
+
+def train(train_labels, train_input_ids, train_attention_masks):
+    batch_size = 32
+    train_dataset = TensorDataset(train_input_ids, train_attention_masks, train_labels)
+
+    train_dataloader = DataLoader(
+        train_dataset,
+        sampler=RandomSampler(train_dataset),
+        batch_size=batch_size
+    )
+
+    model = BertForSequenceClassification.from_pretrained(
+        "bert-base-uncased",
+        num_labels=2,
+        output_attentions=True,
+        output_hidden_states=True,
+    )
+
+    device = get_device()
+    model.to(device)
+    # TODO: learning rate and epsilon
+    optimizer = AdamW(model.parameters())
+
+    epochs = 4
+
+    model.train()
+    for epoch in range(epochs):
+        print("Epoch {} out of {}".format(epoch, epochs))
+        total_train_loss = 0
+        for batch in tqdm(train_dataloader):
+            b_input_ids = batch[0].to(device)
+            b_input_mask = batch[1].to(device)
+            b_labels = batch[2].to(device)
+
+            model.zero_grad()
+            result = model.forward(b_input_ids,
+                                   token_type_ids=None,
+                                   attention_mask=b_input_mask,
+                                   labels=b_labels,
+                                   return_dict=True)
+            loss = result.loss
+            logits = result.logits
+            total_train_loss += loss.item()
+            loss.backward()
+
+            # Avoid the exploding gradient problem:
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+            optimizer.step()
+
+            # TODO: Update the learning rate.
+            # scheduler.step()
+        avg_train_loss = total_train_loss / len(train_dataloader)
+        print("Average train loss is {}".format(avg_train_loss))
+        print("Calculating the train accuracy...")
+
+
+def main():
+    train_labels, train_sentences = load_train_dataset()
+    train_input_ids, train_attention_masks = get_tokens_from_sentences(train_sentences)
+    train_labels = train_labels.astype(np.int)
+    train_labels = np.array(train_labels).reshape(-1, 1)
+    train_labels = torch.Tensor(train_labels).long()
+    print("dtype is ", train_labels.dtype)
+    train(train_labels, train_input_ids, train_attention_masks)
 
 
 if __name__ == '__main__':
     main()
-
