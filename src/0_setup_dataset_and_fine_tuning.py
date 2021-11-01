60df7945124bd0b1978c32096073c835a6845d4e
crisz <crissstian96@gmail.com>
2021-11-01 18:28:35 +0100

    Added validation accuracy

--- src/0_setup_dataset_and_fine_tuning.py
+++ src/0_setup_dataset_and_fine_tuning.py
@@ -2,11 +2,27 @@ import numpy as np
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler
 from tqdm import tqdm
 
-from dataset_utils.load_sst2 import load_train_dataset
+from dataset_utils.load_sst2 import load_train_dataset, load_val_dataset
 from transformers import BertTokenizer, BertForSequenceClassification, AdamW
 import torch
 
 from utils.cuda import get_device
+
+
+def main():
+    train_labels, train_sentences = load_train_dataset()
+    train_input_ids, train_attention_masks = get_tokens_from_sentences(train_sentences)
+
+    train_labels = torch.Tensor(train_labels).long()
+    train_dataset = TensorDataset(train_input_ids, train_attention_masks, train_labels)
+
+    val_labels, val_sentences = load_val_dataset()
+    val_input_ids, val_attention_masks = get_tokens_from_sentences(val_sentences)
+
+    val_labels = torch.Tensor(val_labels).long()
+    val_dataset = TensorDataset(val_input_ids, val_attention_masks, val_labels)
+
+    train(train_dataset=train_dataset, val_dataset=val_dataset)
 
 
 def get_tokens_from_sentences(sentences):
@@ -36,12 +52,23 @@ def get_tokens_from_sentences(sentences):
     return input_ids, attention_masks
 
 
-def train(train_labels, train_input_ids, train_attention_masks):
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+
+def train(train_dataset, val_dataset):
     batch_size = 32
-    train_dataset = TensorDataset(train_input_ids, train_attention_masks, train_labels)
 
     train_dataloader = DataLoader(
         train_dataset,
+        sampler=RandomSampler(train_dataset),
+        batch_size=batch_size
+    )
+
+    val_dataloader = DataLoader(
+        val_dataset,
         sampler=RandomSampler(train_dataset),
         batch_size=batch_size
     )
@@ -88,17 +115,39 @@ def train(train_labels, train_input_ids, train_attention_masks):
             # scheduler.step()
         avg_train_loss = total_train_loss / len(train_dataloader)
         print("Average train loss is {}".format(avg_train_loss))
-        print("Calculating the train accuracy...")
 
+        print("Calculating the val accuracy...")
+        total_eval_loss = 0
+        total_eval_accuracy = 0
+        for batch in val_dataloader:
+            b_input_ids = batch[0].to(device)
+            b_input_mask = batch[1].to(device)
+            b_labels = batch[2].to(device)
 
-def main():
-    train_labels, train_sentences = load_train_dataset()
-    train_input_ids, train_attention_masks = get_tokens_from_sentences(train_sentences)
-    train_labels = train_labels.astype(np.int)
-    train_labels = np.array(train_labels).reshape(-1, 1)
-    train_labels = torch.Tensor(train_labels).long()
-    print("dtype is ", train_labels.dtype)
-    train(train_labels, train_input_ids, train_attention_masks)
+            with torch.no_grad():
+                result = model.forward(b_input_ids,
+                               token_type_ids=None,
+                               attention_mask=b_input_mask,
+                               labels=b_labels,
+                               return_dict=True)
+
+            loss = result.loss
+            logits = result.logits
+
+            # Accumulate the validation loss.
+            total_eval_loss += loss.item()
+
+            # Move logits and labels to CPU
+            logits = logits.detach().cpu().numpy()
+            label_ids = b_labels.to('cpu').numpy()
+
+            # Calculate the accuracy for this batch of test sentences, and
+            # accumulate it over all batches.
+            total_eval_accuracy += flat_accuracy(logits, label_ids)
+
+            # Report the final accuracy for this validation run.
+        avg_val_accuracy = total_eval_accuracy / len(val_dataloader)
+        print("  Accuracy: {0:.2f}".format(avg_val_accuracy))
 
 
 if __name__ == '__main__':
--- src/dataset_utils/load_sst2.py
+++ src/dataset_utils/load_sst2.py
@@ -7,9 +7,19 @@ TRAIN_PATH = DATASET_PATH_BASE / 'sst2' / 'train.tsv'
 TEST_PATH = DATASET_PATH_BASE / 'sst2' / 'test.tsv'
 
 
-def load_train_dataset():
-    train_df = pd.read_csv(TRAIN_PATH, sep='\t', header=None, names=['similarity', 's1'])
+def load_dataset(path):
+    train_df = pd.read_csv(path, sep='\t', header=None, names=['similarity', 's1'])
     train_npy = train_df.values
     train_dataset = np.swapaxes(train_npy, 0, 1)
     train_labels, train_data = train_dataset
+    train_labels = train_labels.astype(np.int)
+    train_labels = np.array(train_labels).reshape(-1, 1)
     return train_labels, train_data
+
+
+def load_train_dataset():
+    return load_dataset(TRAIN_PATH)
+
+
+def load_val_dataset():
+    return load_dataset(TEST_PATH)
