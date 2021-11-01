2694b419821df2af0871920445ae7fa64dd50f10
crisz <crissstian96@gmail.com>
2021-11-01 16:47:37 +0100

    added load dataset

--- dataset/sst2/load_sst2.py
+++ dataset/sst2/load_sst2.py
@@ -1,2 +1,13 @@
-def load_train_npy():
-  pass
+import pandas as pd
+import os
+from pathlib import Path
+
+
+DEV_PATH = Path('.') / 'dev.tsv'
+TRAIN_PATH = Path('.') / 'train.tsv'
+TEST_PATH = Path('.') / 'test.tsv'
+
+
+def load_train_dataset():
+    train_df = pd.read_csv(TRAIN_PATH, sep='\t', header=None, names=['similarity', 's1'])
+    return train_df.values
--- src/0_setup_dataset_and_fine_tuning.py
+++ src/0_setup_dataset_and_fine_tuning.py
@@ -1,5 +1,10 @@
+from dataset.sst2.load_sst2 import load_train_dataset
+
+
 def main():
-    print("Hello world!")
+    train_dataset = load_train_dataset()
+    print(train_dataset[0:5])
+    print(train_dataset.size)
 
 
 if __name__ == '__main__':
