ce816368af36b2d1e6b900a9c9b8083ac7d05dbf
crisz <crissstian96@gmail.com>
2022-02-15 02:21:09 +0100

    improved deepshap

--- src/2_print_shap_values.py
+++ src/2_print_shap_values.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import numpy as np
 import torch
+from datasets import tqdm
 
 from dataset_utils.load_misogyny import load_misogyny_val_dataset
 from explainers.DeepShapWrapper import DeepShapWrapper
@@ -23,24 +24,25 @@ def main():
 
     indices = [180, 289, 324, 425, 485, 523, 568, 779, 817, 972]
 
-    with torch.no_grad():
-        out = model.forward(
-            val_input_ids.to(device),
-            val_attention_masks.to(device),
-            token_type_ids=None,
-            labels=None,
-            return_dict=True,
-            output_attentions=True
-        )
+    embedding = torch.empty((973, 64, 768))
+    for i in tqdm(range(973)):
+        with torch.no_grad():
+            out = model.forward(
+                val_input_ids[i:i+1].to(device),
+                val_attention_masks[i:i+1].to(device),
+                token_type_ids=None,
+                labels=None,
+                return_dict=True,
+                output_attentions=True
+            )
+        embedding[i] = out.embedding_outputs[0]
 
     wrapped = DeepShapWrapper(model=model)
-
-    embedding = out.embedding_outputs
 
     test_embeddings = embedding[indices].to(device)  # [embedding[i] for i in indices]
     test_ids = val_input_ids[indices].to(device)  # [val_input_ids[i] for i in indices]
 
-    e = shap.DeepExplainer(wrapped, embedding[:20])
+    e = shap.DeepExplainer(wrapped, embedding[:200])
     shap_values = e.shap_values(test_embeddings)
 
     s1 = torch.from_numpy(shap_values[1])
